cbdfa644dc81e3d610a57e5a1c80bb18aa3aa61b
msalazarpa <sungsamj12@gmail.com>
2022-01-25 02:58:40 -0500

    detalles varios content

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pagina <code>MauricioSalazar.com.co</code> en construccion
+          Pagina <code> Mauricio Salazar</code> en construccion
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/msalazarpa/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Contacto
         </a>
       </header>
     </div>
